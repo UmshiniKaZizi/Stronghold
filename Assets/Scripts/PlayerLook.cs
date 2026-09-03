0b41498485d79623abba03e3ae3fe93f62f39697
UmshiniKaZizi <mduduzimiya14@gmail.com>
2026-09-03 18:05:20 +0200

    Player Can Look Around

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{   
+    public float mouseSensativity = 50f;
+    public Transform cam;
+    private float xRotation = 0f;
+    private Vector2 lookInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     Cursor.lockState = CursorLockMode.Locked;
+     Cursor.visible = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       HandleMouseLook(); 
+    }
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+    void HandleMouseLook()
+    {
+        float mouseX = lookInput.x * mouseSensativity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensativity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90,90);
+
+        cam.localRotation = Quaternion.Euler(xRotation,0f,0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
