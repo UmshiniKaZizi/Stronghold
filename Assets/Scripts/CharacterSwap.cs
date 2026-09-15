80f4a42662fed91ee0688450cb5b0ba29e2670d6
UnaSwims22 <maswimeunarine@gmail.com>
2026-09-16 00:08:46 +0200

    Created a CharacterSwap script
    Allows player to switch between characters. Need to fix the camera movement though, as the camera only stays on the main character and does not move along with the other characters when swicthed

---
+++ Assets/Scripts/CharacterSwap.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterSwap : MonoBehaviour
+{
+    public Transform character;
+    public List<Transform> possibleCharacters;
+    public int whichCharacter;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (character == null && possibleCharacters.Count >= 1)
+        {
+            character = possibleCharacters[0];
+        }
+        Swap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (whichCharacter == 0)
+            {
+                whichCharacter = possibleCharacters.Count - 1;
+            }
+            else
+            {
+                whichCharacter -= 1;
+            }
+            Swap();
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (whichCharacter == possibleCharacters.Count -1)
+            {
+                whichCharacter = 0;
+            }
+            else
+            {
+                whichCharacter += 1;
+            }
+        }
+        Swap();
+    }
+
+    public void Swap()
+    {
+        character = possibleCharacters[whichCharacter];
+        character.GetComponent<PlayerMovement>().enabled = true;
+        for (int i = 0; i < possibleCharacters.Count; i++)
+        {
+            if (possibleCharacters[i] != character)
+            {
+                possibleCharacters[i].GetComponent<PlayerMovement>().enabled = false;
+            }
+        }
+
+    }
+}
