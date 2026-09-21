909bbf45b91c6867e796e6b810bd87488f78be2e
UmshiniKaZizi <mduduzimiya14@gmail.com>
2026-09-21 17:00:13 +0200

    Revert "Merge branch 'main' into Mdu"
    This reverts commit 940cd2e546c8ea0156fd8c61162a468ae4faa981, reversing
    changes made to 89b4524304297103de8d3358f977bd3a9a6f66b1.

--- Assets/Scripts/CharacterWheelUI.cs
+++ Assets/Scripts/CharacterWheelUI.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class CharacterWheelUI : MonoBehaviour
 {
@@ -13,46 +12,8 @@ public class CharacterWheelUI : MonoBehaviour
     [SerializeField] private Color normalColor = Color.white;
     [SerializeField] private Color selectedColor = Color.yellow;
 
-    [Header("Animation")]
-    [SerializeField] private float selectedScale = 1.2f;
-    [SerializeField] private float normalScale = 1f;
-    [SerializeField] private float animationSpeed = 8f;
-
-    [Header("Selection Sounds")]
-    [SerializeField] private AudioSource audioSource;
-
-    [SerializeField] private AudioClip assaultSound;
-    [SerializeField] private AudioClip artillerySound;
-    [SerializeField] private AudioClip sniperSound;
-
-
-
     public void UpdateCharacterSelection(int selectedCharacter)
     {
-        StartCoroutine(
-            AnimateSlot(
-                assaultSlot,
-                selectedCharacter == 0
-            )
-        );
-
-        StartCoroutine(
-            AnimateSlot(
-                artillerySlot,
-                selectedCharacter == 1
-            )
-        );
-
-        StartCoroutine(
-            AnimateSlot(
-                sniperSlot,
-                selectedCharacter == 2
-            )
-        );
-
-        PlaySelectionSound(selectedCharacter);
-
-
         // Reset all character slots
         assaultSlot.color = normalColor;
         artillerySlot.color = normalColor;
@@ -74,104 +35,15 @@ public class CharacterWheelUI : MonoBehaviour
                 break;
         }
     }
-
-    private void PlaySelectionSound(int selectedCharacter)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (audioSource == null)
-            return;
-
-        switch (selectedCharacter)
-        {
-            case 0:
-                audioSource.PlayOneShot(assaultSound);
-                break;
-
-            case 1:
-                audioSource.PlayOneShot(artillerySound);
-                break;
-
-            case 2:
-                audioSource.PlayOneShot(sniperSound);
-                break;
-        }
+        
     }
 
-    private IEnumerator AnimateSlot(Image slot, bool selected)
+    // Update is called once per frame
+    void Update()
     {
-        Transform slotTransform = slot.transform;
-
-        // Change colour
-        slot.color = selected
-            ? selectedColor
-            : normalColor;
-
-        if (!selected)
-        {
-            // Return inactive characters to normal size
-            slotTransform.localScale =
-                Vector3.one * normalScale;
-
-            yield break;
-        }
-
-        // Selected character starts slightly smaller
-        slotTransform.localScale =
-            Vector3.one * normalScale;
-
-        // Grow
-        yield return ScaleTo(
-            slotTransform,
-            1.25f,
-            0.08f
-        );
-
-        // Shrink slightly
-        yield return ScaleTo(
-            slotTransform,
-            1.12f,
-            0.06f
-        );
-
-        // Settle
-        yield return ScaleTo(
-            slotTransform,
-            selectedScale,
-            0.08f
-        );
+        
     }
-
-    private IEnumerator ScaleTo(
-        Transform target,
-        float targetScale,
-        float duration
-    )
-    {
-        Vector3 startScale = target.localScale;
-
-        Vector3 endScale =
-            Vector3.one * targetScale;
-
-        float time = 0f;
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-
-            float progress =
-                Mathf.Clamp01(time / duration);
-
-            target.localScale = Vector3.Lerp(
-                startScale,
-                endScale,
-                progress
-            );
-
-            yield return null;
-        }
-
-        target.localScale = endScale;
-    }
-
-
-
 }
